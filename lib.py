4411105f00aa3b7f2abb4b976db95ec36691b8e2
Pranav Marla <pranav.a.marla@gmail.com>
2019-08-11 01:09:17 -0400

    Sort stories

--- lib.py
+++ lib.py
@@ -1,5 +1,6 @@
 __author__ = 'Pranav Marla'
 
+from operator import attrgetter
 
 # This generator function returns a generator iterator. Every time the generator iterator is iterated upon, it returns the next consecutive number.
 def consecutive_number_generator_function():
@@ -62,4 +63,24 @@ class Story:
         self.additional_fields = additional_fields
 
     def __repr__(self):
-        return 'Story(id={}, name={}, size={}, priority={}, deadline={}, parents={}, has_children={}, additional_fields={})'.format(self.id, self.name, self.size, self.priority, self.deadline, self.parents, self.has_children, self.additional_fields)
+        return 'Story(id={}, name={}, size={}, priority={}, deadline={}, parents={}, has_children={}, additional_fields={})'.format(self.id, self.name, self.size, self.priority, self.deadline, self.parents, self.has_children, self.additional_fields)
+
+# Sort stories using the following algorithm:
+# Given two stories A and B:
+# If A has a higher priority than B, then A is sorted ahead of B.
+# If their priorities are the same, but A has an earlier deadline than B, then A is sorted ahead of B.
+# If their priorities and deadlines are the same, but A is bigger than B, then A is sorted ahead of B.
+def sort_stories(stories):
+
+    # To get the resulting order we want, we need to sort by the LEAST important attribute first!
+
+    # Sort stories by size in descending order
+    stories.sort(key=attrgetter('size'), reverse=True)
+
+    # Sort resulting stories by deadline in ascending order
+    stories.sort(key=attrgetter('deadline'))
+    
+    # Sort resulting stories by priority in descending order
+    stories.sort(key=attrgetter('priority'), reverse=True)
+
+
