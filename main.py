de4f352eddd17856fe733d3a07086fbe73ca3065
Pranav Marla <pranav.a.marla@gmail.com>
2019-08-11 01:10:03 -0400

    Initial main.py

--- main.py
+++ main.py
@@ -2,4 +2,73 @@
 
 __author__ = 'Pranav Marla'
 
-class 
+
+from datetime import date, datetime
+import lib
+
+Sprint = lib.Sprint
+Story = lib.Story
+
+sprints = \
+    [
+        Sprint(date(2019, 1, 24), 15),
+        Sprint(date(2019, 1, 28), 15)
+    ]
+
+# sprints = \
+#     [
+#         Sprint(date(2019, 1, 24), 20),
+#         Sprint(date(2019, 1, 28), 20)
+#     ]
+
+print('Sprints:')
+for sprint in sprints:
+    print('\t{}'.format(sprint))
+print()
+
+stories = \
+    [
+        Story(id='M', size=1, priority=-1, deadline=date(2019, 1, 28)),
+        Story(id='L', size=3, priority=1, deadline=date(2019, 1, 28)),
+        Story(id='K', size=5, priority=-1, deadline=date(2019, 1, 28)),
+        Story(id='J', size=5, priority=0, deadline=date(2019, 1, 28)),
+        Story(id='I', size=2, priority=-1, deadline=date(2019, 1, 28), parents=['F']),
+        Story(id='H', size=3, priority=1, deadline=date(2019, 1, 28), parents=['F']),
+        Story(id='G', size=3, priority=0, deadline=date(2019, 1, 28), parents=['F']),
+        Story(id='F', size=5, priority=1, deadline=date(2019, 1, 27), has_children=True, parents=['E']),
+        Story(id='E', size=3, priority=1, deadline=date(2019, 1, 26), has_children=True),
+        Story(id='D', size=5, priority=1, deadline=date(2019, 1, 26), parents=['C']),
+        Story(id='C', size=3, priority=1, deadline=date(2019, 1, 25), has_children=True, parents=['B']),
+        Story(id='B', size=2, priority=1, deadline=date(2019, 1, 24), has_children=True),
+        Story(id='A', size=5, priority=1, deadline=date(2019, 1, 28))
+    ]
+
+print('Stories, before sorting:')
+for story in stories:
+    print('\t{}'.format(story))
+print()        
+
+lib.sort_stories(stories)
+
+print('Stories, after sorting:')
+for story in stories:
+    print('\t{}'.format(story))
+print()
+
+overflow_stories = lib.slot_stories(stories, sprints)
+
+print('Sprints, after slotting in stories:')
+for sprint in sprints:
+    print('---')
+    print('Sprint {}:\tCapacity remaining: {}/{}\n'.format(sprint.id, sprint.available_capacity, sprint.total_capacity))
+    for story in sprint.stories:
+        print('\t{}'.format(story.id))
+    print('---\n')
+print()
+
+if overflow_stories:
+    print('The following stories could not be slotted into any sprint:')
+    for story in overflow_stories:
+        print('\t{}'.format(story.id))
+else:
+    print('All stories were successfully slotted into sprints!')
